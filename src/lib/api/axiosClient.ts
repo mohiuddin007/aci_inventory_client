67ee6d0cb9c4f10f659ff3b0bb81b9e336f31ab6
mohiuddin007 <mohiuddinmazumder94@gmail.com>
2025-03-05 04:10:11 +0600

    axios client

--- src/lib/api/axiosClient.ts
+++ src/lib/api/axiosClient.ts
@@ -1,11 +1,26 @@
 import axios from "axios";
+import Cookies from "js-cookie"; 
 
 const axiosClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_REST_API_ENDPOINT, 
+  baseURL: process.env.NEXT_PUBLIC_REST_API_ENDPOINT,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("auth-token"); 
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 axiosClient.interceptors.response.use(
   (response) => response,
